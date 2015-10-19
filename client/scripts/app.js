74537b773cd54e47bc942aecf4254390e71330ff
thedeagler <stefan.wu@gmail.com>
2015-10-19 15:14:50 -0700

    completing init setup of chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,34 @@
 // YOUR CODE HERE:
+var app = {
+  data: null,
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    app.data = Array.prototype.slice.call(data.results);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
+
+// // Posting method
+// app.post(message) = $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/chatterbox',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message');
+//   }
+// });
