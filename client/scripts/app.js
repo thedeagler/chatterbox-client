ac7ca788c321514b527d007b100e776797a27a54
thedeagler <stefan.wu@gmail.com>
2015-10-19 19:22:44 -0700

    working through specs, stuck on chat behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,54 +1,71 @@
 // YOUR CODE HERE:
+
+
 var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
   data: null,
+  clearMessages: function() {
+    $('.chat').children().remove();
+  },
+  addMessage: function(messageObj) {
+    var username = xssFilters.inHTMLData(messageObj["username"]);
+    var text = xssFilters.inHTMLData(messageObj["text"]);
+    // Use the escape function here on text and username
+    //Convert to html element
+    $('.chat').append("<div class='row message'>"
+          + "<div class='username col-sm-3 col-md-3 col-lg-3'>" 
+          + username 
+          + "</div>"
+          + "<div class='col-sm-9 col-md-9 col-lg-9 text'>" 
+          + text 
+          + "</div>"
+        + "</div>"
+        );
+  },
+  fetch: function() {
+    // Get messages
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/jsonp',
+      success: function (data) {
+        app.data = Array.prototype.slice.call(data.results);
+        // toHtml(app.data[0]);
+        app.data.forEach(function(el) {
+          app.addMessage(el);
+        });
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
+  },
+  send: function(message) {
+    // Posting method
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  init: function() {
+    app.fetch();
+  },
 };
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  contentType: 'application/jsonp',
-  success: function (data) {
-    app.data = Array.prototype.slice.call(data.results);
-    // toHtml(app.data[0]);
-    loadAll(app.data)
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
-
-// Take data and convert into an html element
-function toHtml(messageObj) {
-  var text = xssFilters.inHTMLData(messageObj.text);
-  var username = xssFilters.inHTMLData(messageObj.username);
-  // Use the escape function here on text and username
-  //Convert to html element
-  $('.chat').append("<div class='message'><span class='username'>" + username + "</span>: <span class='text'>" + text + "</span><div>");
-}
 
 function loadAll(array) {
   array.forEach(function(el) {
-    toHtml(el);
+    app.addMessage(el);
   })
 }
 
-// Security escape function
-
-
-// // Posting method
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify({"username": "wordToYourMother", "text": "This is the time, son: ".concat(new Date())}),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -69,9 +69,9 @@ describe('chatterbox', function() {
 
     describe('chatroom behavior', function() {
       it('should be able to clear messages from the DOM', function(){
-        var orig = $('#chats').html('<blink>OMG IT\'s 1998!</blink>');
+        var orig = $('.chat').html('<blink>OMG IT\'s 1998!</blink>');
         app.clearMessages();
-        expect($('#chats').children().length).to.equal(0);
+        expect($('.chat').children().length).to.equal(0);
       });
 
       it('should be able to add messages to the DOM', function(){
@@ -83,7 +83,7 @@ describe('chatterbox', function() {
 
         app.addMessage(message);
 
-        expect($('#chats').children().length).to.equal(1);
+        expect($('.chat').children().length).to.equal(1);
       });
 
       it('should be able to add rooms to the DOM', function(){
